a1c1b2399be07103042e46f48c95d9af8fb065b2
PessahDerek <pesaderek1738@gmail.com>
2025-01-31 18:40:06 +0300

    update

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const baseUrl = window.location.host.includes("local")
     ? "http://localhost:8000"
-    : "http://10.11.1.75:8000"
-    // : "https://api.soleguardalliance.org" // todo: to be rectified
+    // : "http://10.11.1.75:8000"
+    : "https://api.soleguardalliance.org" // todo: to be rectified
 
 const endpoint = axios.create({
     baseURL: baseUrl
